28e9598ca480a02eed9d311bbe91aa0128744c1f
Michal Hoftich <michal.h21@gmail.com>
2017-01-30 10:36:14 +0100

    Pročistit jména autorů

--- autori-publikace.lua
+++ autori-publikace.lua
@@ -1,30 +1,15 @@
-local function load_tsv(filename, skip_first)
-  local skip_first = skip_first
-  local t = {}
-  local labels = {}
-  for line in io.lines( filename) do
-    local i = 1
-    local l = {} 
-    for m in line:gmatch("([^%\t]+)") do
-      l[labels[i] or i] = m
-      i = i + 1
-    end
-    if not skip_first then
-      t[#t+1] = l
-    else
-      -- labels = l
-    end
-    skip_first = false
-  end
-  return t
-end
+
+local load_tsv = require "load_tsv"
 
 local function remove_dupl(t)
   local ids = {}
   local new = {}
+  local i = 0
   for k, x in ipairs(t) do
     local id = x[2]
     if ids[id] then
+      i = i + 1
+      -- print(i, "Duplikát", id)
     else 
       new[#new+1] = x
     end
@@ -51,6 +36,7 @@ local function parse_auth(s)
       -- print(aut)
       local autor, id, kat, vykaz = aut:match("([^%(]+)%(([0-9]+).* – ([^%(]+)%(Vykaz%.:([^%)]+)")
       -- print(autor, id, kat, vykaz)
+      autor = autor:gsub("^%s*%[G%]", ""):gsub("^%s*", ""):gsub("%s*$", "")
       result.autor = autor
       result.id = id
       result.katedry = parse_kat(kat)
@@ -66,46 +52,131 @@ local function get_pubauthors(x)
 end 
 
 local function get_type(typ, druh, jazyk)
-  print(typ, druh, jazyk)
+  local types = {["KAPITOLA V KNIZE"] = "BC",
+  KNIHA = "B"}
   if druh:match "česká" or druh:match "zahraniční" then
     jazyk = druh
     druh = ""
   end
   if druh:match("[%a]") then return druh end
+  typ = (types[typ] or typ) .. "-".. jazyk
+  -- print(typ, druh, jazyk)
   return typ
 end
 
-local l = remove_dupl(load_tsv(arg[1], true))
-
-local ignore_kat = {PedF= true, Dohody = true}
-
-for _, v in pairs(l) do
-  local id = v[2]
-  local typ = v[3]
-  local druh = v[10]
-  local jazyk  = v[11]
-  local autori = get_pubauthors(v) 
-  local bodydiv = #autori 
-  for k, v in ipairs(autori) do
-    local count = v.katedry or {}
-    local i = 0
-    local katedra = ""
-    for _, kat in ipairs(count) do
-      if not ignore_kat[kat.katedra] and kat.fakulta == "PedF"  then
-        i = i + 1
-        katedra = kat.katedra 
-      end
+local function get_author(v)
+  local ignore_kat = {PedF= true, Dohody = true}
+  local count = v.katedry or {}
+  local i = 0
+  local katedra = ""
+  for _, kat in ipairs(count) do
+    if not ignore_kat[kat.katedra] and kat.fakulta == "PedF"  then
+      i = i + 1
+      katedra = kat.katedra 
     end
-    if i > 1 then 
-      print "Autor vykazuje moc kateder"
-      for _, kat in ipairs(count) do
-        print(id, v.autor, kat.fakulta, kat.katedra)
+  end
+  -- if i > 1 then 
+  --   print "Autor vykazuje moc kateder"
+  --   for _, kat in ipairs(count) do
+  --     print(id, v.autor, kat.fakulta, kat.katedra)
+  --   end
+  -- elseif i == 0  and #count > 0 then
+  --   print "Nenašel jsem žádného autora"
+  -- end
+  return katedra, i, count
+end
+
+
+
+
+-- local pubtypes = {}
+local function make_log(l)
+  local log = {}
+  for i, v in pairs(l) do
+    local id = v[2]
+    local typ = v[3]
+    local druh = v[10]
+    local jazyk  = v[11]
+    local autori = get_pubauthors(v) 
+    local bodydiv = #autori 
+    local autorcount = 0
+    for k, v in ipairs(autori) do
+      local katedra, poc_kateder, poc_xxx = get_author(v)
+      local body = 1 / bodydiv
+      local pub_type = get_type(typ, druh, jazyk)
+      if v.autor then
+        autorcount = autorcount + 1
+        log[#log + 1] = {autor = v.autor, katedra = katedra, typ = pub_type, body = body, id =  id}
       end
-    elseif i == 0  and #count > 0 then
-      print "Nenašel jsem žádného autora"
+      -- print(id,  v.autor, katedra, pub_type , body)
     end
-    local body = 1 / bodydiv
-    local pub_type = get_type(typ, druh, jazyk)
-    print(id,  v.autor, katedra, pub_type , body)
+    if autorcount == 0 then
+      print("No authors", id)
+    else
+      -- print(i, "Pocet autoru", autorcount, bodydiv,  id)
+    end
+  end
+  return log
+end
+
+local function  get_pubtypes(log)
+  local pubcount = {}
+  local pubtypes = {}
+  for _, x in ipairs(log) do
+    local pub_type = x.typ
+    pubcount[pub_type] = (pubcount[pub_type] or 0) + 1
+  end
+  for k,_ in pairs(pubcount) do
+    pubtypes[#pubtypes+1] = k
+  end
+  table.sort(pubtypes)
+  return pubtypes,pubcount
+end
+
+local function make_pubtable(log, pubtypes)
+  local function prepare()
+    local t = {}
+    for _,k in pairs(pubtypes) do
+      t[k] = 0
+    end
+    return t
+  end
+  local katedry = {}
+  for _, rec in ipairs(log) do
+    local katedra = rec.katedra
+    local typ = rec.typ
+    local body = rec.body
+    local k = katedry[katedra] or prepare()
+    k[typ] = k[typ] + body
+    katedry[katedra] = k
+  end
+  return katedry
+end
+    
+local function print_pubtable(tbl, pubtypes)
+  print("", table.concat(pubtypes, "\t"))
+  for katedra, rec in pairs(tbl) do
+    local t = {katedra}
+    for _,label in ipairs(pubtypes) do
+      t[#t+1] = rec[label] or 0
+    end
+    print(table.concat(t, "\t"))
   end
 end
+      
+
+
+local l = remove_dupl(load_tsv(arg[1], true))
+local log = make_log(l)
+
+for i, k in ipairs(log) do
+  print(k.autor, k.katedra, k.typ, k.body, k.id)
+end
+
+local pubtypes, pubcount = get_pubtypes(log)
+-- for k,v in pairs(pubtypes) do 
+--   print(k,v)
+-- end
+local kat_table = make_pubtable(log, pubtypes)
+print_pubtable(kat_table, pubtypes)
+
